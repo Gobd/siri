370404b255b854ac321ca324c49d6101f8e26fa4
Brian Kemper <bkemper@gmail.com>
2016-03-02 13:29:27 -0700

    done

--- index.js
+++ index.js
@@ -1,36 +1,73 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var users = [{
-  name: 'Guy man',
-  city: 'NY City'
-},{
-  name: 'person alp',
-  city: 'good city'
-}];
-
 var app = express();
-//without a path this will for all paths
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-//will send ok status and the contents of the users var
-app.get('/users', function(req, res, next){
-  res.status(200).json(users);
+var messages = ["Hello there.", "I'm sorry, I cannot take any requests at this time.", "I can tell you how to do that."];
+
+function getRandomMessage(){
+  var length = messages.length;
+  var rand = Math.floor(Math.random()*length);
+  return messages[rand];
+}
+
+
+app.get('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify({
+    message: getRandomMessage()
+  }));
 });
 
-//if method is get and path is users do this
-//by responding in the manner rather than with req.body they will recieve anything the DB added to the object when it was written
-app.post('/users', function(req, res, next){
-  users.push(req.body);
-  res.status(200).json(users[users.length - 1]);
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send();
 });
 
-//will delete last user and return the deleted user
-app.delete('/users', function(req, res, next){
-  var del = users.pop();
-  res.status(200).json(del);
-});
+// var users = [{
+//   name: 'Guy man',
+//   city: 'NY City'
+// },{
+//   name: 'person alp',
+//   city: 'good city'
+// }];
 
-app.listen(3000, function(){
-  console.log('Listening on port 3000');
+// without a path this will for all paths
+// app.use(bodyParser.json());
+//
+// //will send ok status and the contents of the users var
+// app.get('/users', function(req, res, next){
+//   res.status(200).json(users);
+// });
+//
+// if method is get and path is users do this
+// by responding in the manner rather than with req.body they will recieve anything the DB added to the object when it was written
+// app.post('/users', function(req, res, next){
+//   users.push(req.body);
+//   res.status(200).json(users[users.length - 1]);
+// });
+//
+// will delete last user and return the deleted user
+// app.delete('/users', function(req, res, next){
+//   var del = users.pop();
+//   res.status(200).json(del);
+// });
+
+app.listen(8887, function(){
+  console.log('Listening on port 8887');
 });
